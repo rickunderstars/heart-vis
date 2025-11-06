20a7914a553c008d7f88d178b0e7065ae3517987
riki <rickunderstars@gmail.com>
2025-11-07 00:30:17 +0100

    commentato tutto main.cpp

--- src/web/main.js
+++ src/web/main.js
@@ -19,11 +19,6 @@ renderer.setAnimationLoop(animate);
 
 viewport.append(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
 camera.position.z = 5;
 
 function animate() {
@@ -34,26 +29,29 @@ function animate() {
 
 /////// model upload ///////
 
-HeartModule().then(cpp => {
-	const rawMeshElement = document.getElementById('raw-mesh');
-	const fileElement = document.getElementById('filename');
+HeartModule().then((cpp) => {
+	const rawMeshElement = document.getElementById("raw-mesh");
+	const fileElement = document.getElementById("filename");
 
-	rawMeshElement.addEventListener('change', function(event) {
+	rawMeshElement.addEventListener("change", function (event) {
 		if (event.target.files.length > 0) {
 			const file = event.target.files[0];
 			fileElement.textContent = file.name;
 
 			const reader = new FileReader();
 
-			reader.onload = function(e) {
+			reader.onload = function (e) {
 				const fileContent = e.target.result;
 				const mesh = cpp.importMesh(fileContent);
-				console.log(mesh.toPlyStringDefault());
+				// TODO: continue
+				const geometry = new THREE.BoxGeometry(1, 1, 1);
+				const material = new THREE.MeshBasicMaterial();
+				const cube = new THREE.Mesh(geometry, material);
+				scene.add(cube);
 			};
 			reader.readAsText(file);
 		} else {
-			fileElement.textContent = 'No file selected';
+			fileElement.textContent = "No file selected";
 		}
 	});
-
 });
