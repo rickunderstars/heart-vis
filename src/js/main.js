7ff2a34b63d4129376d025a5d71a3d7baa9343e5
riki <rickunderstars@gmail.com>
2025-12-09 02:28:27 +0100

    spostato codice di scena e luci in scene.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { getMax, get2Min } from "./utils/mathUtils.js";
+import { getMax, get2Min } from "./utils/math-utils.js";
+import { initScene } from "./core/scene.js";
 /////// load shaders ///////
 let vShader = null;
 let fShader = null;
@@ -40,7 +41,7 @@ try {
 
 /////// three.js ///////
 
-var scene = new THREE.Scene();
+var scene = initScene();
 var viewport = document.getElementById("viewport");
 var camera = new THREE.PerspectiveCamera(
 	50,
@@ -72,17 +73,6 @@ window.addEventListener("resize", onViewportResize);
 window.addEventListener("mousemove", onMouseMove, false);
 
 camera.position.z = 5;
-
-const light1 = new THREE.DirectionalLight(0xffffff, 1);
-const light2 = new THREE.DirectionalLight(0xffffff, 1);
-const light3 = new THREE.DirectionalLight(0xffffff, 1);
-const light4 = new THREE.DirectionalLight(0xffffff, 1);
-light1.position.set(100, 100, 100);
-light2.position.set(-100, -100, -100);
-light3.position.set(100, -100, 100);
-light4.position.set(100, 100, -100);
-scene.add(light1, light2, light3, light4);
-scene.add(new THREE.AmbientLight(0xffffff, 3));
 
 const clock = new THREE.Clock();
 
