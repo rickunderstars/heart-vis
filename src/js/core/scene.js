6dd5e78d3c7cfe008da281cf7e849c45281ef261
riki <rickunderstars@gmail.com>
2025-12-09 18:47:48 +0100

    spostata funzione vertexPicker nel proprio file

--- src/js/core/scene.js
+++ src/js/core/scene.js
@@ -3,6 +3,10 @@ import * as THREE from "three";
 export function initScene() {
 	const scene = new THREE.Scene();
 
+	return scene;
+}
+
+export function initLights(scene) {
 	const light1 = new THREE.DirectionalLight(0xffffff, 1);
 	const light2 = new THREE.DirectionalLight(0xffffff, 1);
 	const light3 = new THREE.DirectionalLight(0xffffff, 1);
@@ -11,8 +15,7 @@ export function initScene() {
 	light2.position.set(-100, -100, -100);
 	light3.position.set(100, -100, 100);
 	light4.position.set(100, 100, -100);
+
 	scene.add(light1, light2, light3, light4);
 	scene.add(new THREE.AmbientLight(0xffffff, 3));
-
-	return scene;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { getMax, get2Min } from "./utils/math-utils.js";
-import { initScene } from "./core/scene.js";
+import { initLights, initScene } from "./core/scene.js";
 import { createRenderer } from "./core/renderer.js";
 import {
 	state,
@@ -14,6 +14,7 @@ import {
 	setupBrowseFile,
 	setupDragAndDrop,
 } from "./interaction/file-handler.js";
+import { vertexPicker } from "./interaction/vertex-picker.js";
 
 /////// load shaders ///////
 export let vShader = null;
@@ -59,6 +60,7 @@ try {
 /////// three.js ///////
 
 export const scene = initScene();
+initLights(scene);
 const viewport = document.getElementById("viewport");
 export const camera = new THREE.PerspectiveCamera(
 	50,
@@ -70,7 +72,6 @@ const renderer = createRenderer(viewport);
 viewport.append(renderer.domElement);
 
 const mouse = new THREE.Vector2();
-const raycaster = new THREE.Raycaster();
 
 export const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener("change", () => {
@@ -156,83 +157,7 @@ function onMouseMove(e) {
 	const rect = renderer.domElement.getBoundingClientRect();
 	mouse.x = ((e.clientX - rect.left) / rect.width) * 2 - 1;
 	mouse.y = -(((e.clientY - rect.top) / rect.height) * 2 - 1);
-	vertexPicker();
-}
-
-function vertexPicker() {
-	if (state.activeMesh === -1 || !getActiveMesh()) {
-		document.getElementById("vertex-info").innerHTML = "no mesh loaded";
-		return;
-	}
-	raycaster.setFromCamera(mouse, camera);
-	const intersects = raycaster.intersectObject(getActiveMesh().mesh);
-
-	if (intersects.length > 0) {
-		const firstHit = intersects[0];
-		const face = firstHit.face;
-		const geometry = firstHit.object.geometry;
-		const positions = geometry.attributes.position;
-		const object = firstHit.object;
-
-		const v0 = new THREE.Vector3().fromBufferAttribute(positions, face.a);
-		const v1 = new THREE.Vector3().fromBufferAttribute(positions, face.b);
-		const v2 = new THREE.Vector3().fromBufferAttribute(positions, face.c);
-
-		v0.applyMatrix4(object.matrixWorld);
-		v1.applyMatrix4(object.matrixWorld);
-		v2.applyMatrix4(object.matrixWorld);
-
-		const bary = new THREE.Vector3();
-		THREE.Triangle.getBarycoord(firstHit.point, v0, v1, v2, bary);
-
-		const active = getActiveMesh().valueSets;
-
-		const unipolar =
-			active.unipolar[face.a] * bary.x +
-			active.unipolar[face.b] * bary.y +
-			active.unipolar[face.c] * bary.z;
-
-		const bipolar =
-			active.bipolar[face.a] * bary.x +
-			active.bipolar[face.b] * bary.y +
-			active.bipolar[face.c] * bary.z;
-
-		const lat =
-			active.lat[face.a] * bary.x +
-			active.lat[face.b] * bary.y +
-			active.lat[face.c] * bary.z;
-
-		let intValuesIndex = face.a;
-		if (bary.y > bary.x && bary.y > bary.z) {
-			intValuesIndex = face.b;
-		} else if (bary.z > bary.x && bary.z > bary.y) {
-			intValuesIndex = face.c;
-		}
-
-		const eml = active.eml[intValuesIndex];
-		const exteml = active.exteml[intValuesIndex];
-		const scar = active.scar[intValuesIndex];
-		const groupid = active.groupid[intValuesIndex];
-
-		document.getElementById("vertex-info").innerHTML =
-			"<div class='vertex-info'>Unipolar: " +
-			unipolar.toFixed(3) +
-			"</br>Bipolar: " +
-			bipolar.toFixed(3) +
-			"</br>LAT: " +
-			lat.toFixed(3) +
-			"</br>EML: " +
-			eml +
-			"</br>ExtEML: " +
-			exteml +
-			"</br>SCAR: " +
-			scar +
-			"</br>groupID: " +
-			groupid +
-			"</div>";
-	} else {
-		document.getElementById("vertex-info").innerHTML = "";
-	}
+	vertexPicker(state, mouse, camera);
 }
 
 function setData(meshIndex, dataSet) {
