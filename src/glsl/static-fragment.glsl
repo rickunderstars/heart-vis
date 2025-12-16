ab8f0d296ced21d5632eacce55bb0e47fff57819
riki <rickunderstars@gmail.com>
2025-12-16 09:22:40 +0100

    luci direzionali per shader statico aggiornate

--- src/glsl/static-fragment.glsl
+++ src/glsl/static-fragment.glsl
@@ -24,17 +24,19 @@ vec3 TurboColormap(in float x) {
 }
 
 void main() {
-	vec3 topRightDir = normalize(vec3(1.0, 1.0, 1.0));
-	vec3 topLeftDir = normalize(vec3(-1.0, -1.0, 1.0));
-	vec3 frontDir = normalize(vec3(0.0, 0.0, 1.0));
+	vec3 light1Dir = normalize(vec3(-1.0, 1.0, 1.0));
+	vec3 light2Dir = normalize(vec3(1.0, 1.0, 1.0));
+	vec3 light3Dir = normalize(vec3(1.0, -1.0, 1.0));
+	vec3 light4Dir = normalize(vec3(-1.0, -1.0, 1.0));
 
-	float topRightDiffuse = max(dot(vNormal, topRightDir), 0.0);
-	float topLeftDiffuse = max(dot(vNormal, topLeftDir), 0.0);
-	float frontDiffuse = max(dot(vNormal, frontDir), 0.0);
+	float light1Diffuse = max(dot(vNormal, light1Dir), 0.0);
+	float light2Diffuse = max(dot(vNormal, light2Dir), 0.0);
+	float light3Diffuse = max(dot(vNormal, light3Dir), 0.0);
+	float light4Diffuse = max(dot(vNormal, light4Dir), 0.0);
 
-	vec3 lambert = topRightDiffuse * vec3(1.2) + topLeftDiffuse * vec3(1.2) +
-				   frontDiffuse * vec3(0.6);
-	lambert = lambert / vec3(3.0);
+	vec3 lambert = light1Diffuse * vec3(1.2) + light2Diffuse * vec3(1.2) +
+				   light3Diffuse * vec3(0.8) + light4Diffuse * vec3(0.8);
+	lambert = lambert / vec3(4.0);
 
 	vec3 nullColor = vec3(0.5, 0.5, 0.5);
 	vec3 color = TurboColormap(val);
