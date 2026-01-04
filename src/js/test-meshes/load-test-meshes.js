27923138e9a4364e0b21a4dcece3e5f1098b5a21
riki <rickunderstars@gmail.com>
2026-01-04 19:19:07 +0100

    separata logica di file handling e file processing

---
+++ src/js/test-meshes/load-test-meshes.js
@@ -0,0 +1,2 @@
+import testMesh1 from "@assets/test-meshes/2-LA.mesh?raw";
+import testMesh2 from "@assets/test-meshes/2-LA-FA.mesh?raw";
--- src/js/visualization/scene.js
+++ src/js/visualization/scene.js
@@ -5,19 +5,3 @@ export function createScene() {
 
 	return scene;
 }
-
-export function initLights(scene) {
-	/*
-	const light1 = new THREE.DirectionalLight(0xffffff, 1);
-	const light2 = new THREE.DirectionalLight(0xffffff, 1);
-	const light3 = new THREE.DirectionalLight(0xffffff, 1);
-	const light4 = new THREE.DirectionalLight(0xffffff, 1);
-	light1.position.set(100, 100, 100);
-	light2.position.set(-100, -100, -100);
-	light3.position.set(100, -100, 100);
-	light4.position.set(100, 100, -100);
-
-	scene.add(light1, light2, light3, light4);
-	scene.add(new THREE.AmbientLight(0xffffff, 3));
-	*/
-}
