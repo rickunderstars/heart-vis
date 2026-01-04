9e546cfa218a596e0a529e70f9715ecef22cf510
riki <rickunderstars@gmail.com>
2026-01-04 19:36:44 +0100

    separata logica di file processing e update della lista di mesh caricate

--- src/js/interaction/file-processing.js
+++ src/js/interaction/file-processing.js
@@ -6,12 +6,12 @@ export function processFile(dependencies) {
 		dependencies;
 
 	const fileElement = document.getElementById("filename");
-	HeartModule().then((cpp) => {
-		if (state.meshes.some((item) => item.filename === file.name)) {
-			console.log("Mesh already uploaded");
-			return;
-		}
+	if (state.meshes.some((item) => item.filename === file.name)) {
+		console.log("Mesh already uploaded");
+		return;
+	}
 
+	HeartModule().then((cpp) => {
 		const reader = new FileReader();
 
 		reader.onload = function (e) {
@@ -25,25 +25,6 @@ export function processFile(dependencies) {
 				return;
 			}
 			fileElement.innerHTML = "Last upload: " + file.name;
-
-			/// -- test state.meshes fixes -- ///
-
-			if (file.name === "2-LA.mesh") {
-				mesh.triangleFix(8703, 4559, 4538);
-				mesh.fixNMEdges();
-			} else if (file.name === "2-LA-FA.mesh") {
-				mesh.triangleFix(25180, 12810, 12813);
-				mesh.triangleFix(29108, 9930, 14703);
-				mesh.triangleFix(21420, 10857, 10941);
-				mesh.triangleFix(56, 38, 29);
-				mesh.triangleFix(30812, 15492, 15447);
-				mesh.triangleFix(30578, 14384, 14398);
-				let fixTri = new cpp.Triangle(15417, 14398, 14381);
-				mesh.triangles.push_back(fixTri);
-				mesh.fixNMEdges();
-			}
-
-			// -- end fix -- //
 
 			const vertices = mesh.Float32ArrayOfVertices();
 			const triangles = mesh.Uint32ArrayOfTriangles();
@@ -101,35 +82,7 @@ export function processFile(dependencies) {
 
 			renderer.render(scene, camera);
 
-			let meshValue = 0;
-			document.getElementById("loaded-meshes").innerHTML = "";
-			for (const m of state.meshes) {
-				let corners = "";
-				let checked = "";
-				if (state.activeMesh === 0 && state.meshes.length - 1 === 0) {
-					corners = " class='mesh-top mesh-bottom' ";
-					checked = "checked";
-				} else if (meshValue === 0) {
-					corners = " class='mesh-top' ";
-				} else if (state.activeMesh === state.meshes.length - 1) {
-					corners = " class='mesh-bottom' ";
-					checked = "checked";
-				}
-				document.getElementById("loaded-meshes").innerHTML +=
-					"<label" +
-					corners +
-					">" +
-					"<input type='radio' name='loaded-mesh' value='" +
-					meshValue +
-					"' " +
-					checked +
-					"/>" +
-					"<span>" +
-					m.filename +
-					"</span>" +
-					"</label¨>";
-				meshValue++;
-			}
+			updateMeshesList(state);
 
 			console.log(
 				"Mesh loaded successfully. Meshes loaded:",
@@ -138,4 +91,36 @@ export function processFile(dependencies) {
 		};
 		reader.readAsText(file);
 	});
+}
+
+function updateMeshesList(state) {
+	let meshValue = 0;
+	document.getElementById("loaded-meshes").innerHTML = "";
+	for (const m of state.meshes) {
+		let corners = "";
+		let checked = "";
+		if (state.activeMesh === 0 && state.meshes.length - 1 === 0) {
+			corners = " class='mesh-top mesh-bottom' ";
+			checked = "checked";
+		} else if (meshValue === 0) {
+			corners = " class='mesh-top' ";
+		} else if (state.activeMesh === state.meshes.length - 1) {
+			corners = " class='mesh-bottom' ";
+			checked = "checked";
+		}
+		document.getElementById("loaded-meshes").innerHTML +=
+			"<label" +
+			corners +
+			">" +
+			"<input type='radio' name='loaded-mesh' value='" +
+			meshValue +
+			"' " +
+			checked +
+			"/>" +
+			"<span>" +
+			m.filename +
+			"</span>" +
+			"</label¨>";
+		meshValue++;
+	}
 }
--- src/js/test-meshes/load-test-meshes.js
+++ src/js/test-meshes/load-test-meshes.js
@@ -1,2 +1,19 @@
 import testMesh1 from "@assets/test-meshes/2-LA.mesh?raw";
 import testMesh2 from "@assets/test-meshes/2-LA-FA.mesh?raw";
+
+/*
+if (file.name === "2-LA.mesh") {
+	mesh.triangleFix(8703, 4559, 4538);
+	mesh.fixNMEdges();
+} else if (file.name === "2-LA-FA.mesh") {
+	mesh.triangleFix(25180, 12810, 12813);
+	mesh.triangleFix(29108, 9930, 14703);
+	mesh.triangleFix(21420, 10857, 10941);
+	mesh.triangleFix(56, 38, 29);
+	mesh.triangleFix(30812, 15492, 15447);
+	mesh.triangleFix(30578, 14384, 14398);
+	let fixTri = new cpp.Triangle(15417, 14398, 14381);
+	mesh.triangles.push_back(fixTri);
+	mesh.fixNMEdges();
+}
+*/
